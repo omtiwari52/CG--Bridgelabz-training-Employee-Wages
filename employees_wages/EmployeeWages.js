48bffd01a201aca630de0da1d8ed36fc489867e3
omtiwari52 <tiwariom900@gmail.com>
2025-03-03 22:40:52 +0530

    [Om Tiwari] refactor: code refactored📦

--- employees_wages/EmployeeWages.js
+++ employees_wages/EmployeeWages.js
@@ -2,9 +2,9 @@
 
 const IS_ABSENT = 0;
 
-let is_Present = Math.floor(Math.random() * 10) % 2;  //this will return either 0 or 1 i.e true or false
+let empCheck = Math.floor(Math.random() * 10) % 2;  //this will return either 0 or 1 i.e true or false
 const employeeCheck = () => {
-    if(is_Present === IS_ABSENT){
+    if(empCheck === IS_ABSENT){
         console.log("Employee is Absent!!");
     }else{
         console.log("Employee is Present!!");
