726b7a495bd32f40e53bf7ac2bd34deea0961be2
omtiwari52 <tiwariom900@gmail.com>
2025-03-04 13:29:54 +0530

    [Om Tiwari] add: Added name validation regix and error handling✨

--- employees_wages/EmployeePayroll.js
+++ employees_wages/EmployeePayroll.js
@@ -2,10 +2,22 @@
 class EmployeePayroll {
     constructor(id, name, salary, gender, startDate) {
         this.id = id;
-        this.name = name;
         this.salary = salary;
         this.gender = gender;
         this.startDate = new Date(startDate);
+        this.setName(name);
+    }
+
+    setName(name) {
+        try {
+            const nameRegex = /^[A-Z][a-zA-Z]{2,}(?: [A-Z][a-zA-Z]{1,})*$/;
+            if (!nameRegex.test(name)) {
+                throw new Error("Invalid Name! Name must start with a capital letter and have at least 3 characters.");
+            }
+            this.name = name;
+        } catch (error) {
+            console.error(error.message);
+        }
     }
 
    
@@ -13,8 +25,11 @@ class EmployeePayroll {
         `ID: ${this.id}, Name: ${this.name}, Salary: $${this.salary}, Gender: ${this.gender}, Start Date: ${this.startDate.toDateString()}`;
 }
 
+
 const emp1 = new EmployeePayroll(101, "HarshVeer", 150, "Trans", "2023-05-10");
-const emp2 = new EmployeePayroll(102, "Om Tiwari", 9000000, "Male", "2022-11-15");
+const emp2 = new EmployeePayroll(102, "Omm Tiwari", 9000000, "Male", "2022-11-15");
+const emp3 = new EmployeePayroll(103, "om", 600000, "Male", "2024-01-20"); 
 
 console.log(emp1.getDetails());
-console.log(emp2.getDetails());
+console.log(emp2.getDetails()); 
+console.log(emp3.getDetails()); 
