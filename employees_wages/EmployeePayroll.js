f4077141223196f39524ed78fab5e5ade92d8b02
omtiwari52 <tiwariom900@gmail.com>
2025-03-04 13:04:56 +0530

    [Om Tiwari] add: extended employe payroll to store gender and starting date✨

--- employees_wages/EmployeePayroll.js
+++ employees_wages/EmployeePayroll.js
@@ -1,15 +1,20 @@
+
 class EmployeePayroll {
-    constructor(id, name, salary) {
+    constructor(id, name, salary, gender, startDate) {
         this.id = id;
         this.name = name;
         this.salary = salary;
+        this.gender = gender;
+        this.startDate = new Date(startDate);
     }
 
-    getDetails = () => `ID: ${this.id}, Name: ${this.name}, Salary: $${this.salary}`;
+   
+    getDetails = () => 
+        `ID: ${this.id}, Name: ${this.name}, Salary: $${this.salary}, Gender: ${this.gender}, Start Date: ${this.startDate.toDateString()}`;
 }
 
-const emp1 = new EmployeePayroll(101, "HarshVeer", 150);
-const emp2 = new EmployeePayroll(102, "Om TIwari", 9000000);
+const emp1 = new EmployeePayroll(101, "HarshVeer", 150, "Trans", "2023-05-10");
+const emp2 = new EmployeePayroll(102, "Om Tiwari", 9000000, "Male", "2022-11-15");
 
 console.log(emp1.getDetails());
 console.log(emp2.getDetails());
