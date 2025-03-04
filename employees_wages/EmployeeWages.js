5ee761aec5106f8e4fc95fda3f47e722b7fce96a
omtiwari52 <tiwariom900@gmail.com>
2025-03-04 11:24:37 +0530

    [Om Tiwari] add: Ability to store the day, hours worked and wage earned in a single object feature added✨

--- employees_wages/EmployeeWages.js
+++ employees_wages/EmployeeWages.js
@@ -232,3 +232,45 @@ empDailyWageMap.forEach((value, key, map) => {
 console.log("Full Working Days: "+ fullWorkingDays);
 console.log("Part Working Days: "+ partWorkingDays);
 console.log("Non Working Days: "+ nonWorkingDays);
+
+
+// UC 10 Object Creation 
+
+let totalEmpHrs = 0;
+
+totalWorkingDays = 0;
+
+let empDailyHrsAndWageArr = new Array();
+
+while (
+  totalEmpHrs <= MAX_HRS_IN_MONTH &&
+  totalWorkingDays < NUM_OF_WORKING_DAYS
+) {
+  totalWorkingDays++;
+
+  let empCheck = Math.floor(Math.random() * 10) % 3;
+
+  let empHrs = getWorkingHours(empCheck);
+
+  totalEmpHrs += empHrs;
+
+  empDailyHrsAndWageArr.push({
+    dayNum: totalWorkingDays,
+    dailyHours: empHrs,
+    dailywage: calcDailyWage(empHrs),
+
+    toString() {
+      return (
+        "\nDay" +
+        this.dayNum +
+        "=> Working Hours is " +
+        this.dailyHours +
+        " And Wage Earned + this.dailywage"
+      );
+    },
+  });
+}
+
+console.log(
+  "UC 10 Showing Daily Hours Worked and Wage Earned: " + empDailyHrsAndWageArr
+);
